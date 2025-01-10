cd25c9d880a34c7bf768dbd515cda22b452c48a4
aviation5770 <aviation5770@gmail.com>
2025-01-10 18:41:16 +0900

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import React from 'react'
-import './App.css';
+import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <div />
-  );
-}
-
-const Wrapper = () => {
   return (
     <>
       <Header />
     </>
   );
-};
+}
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import { UseNavigate } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
