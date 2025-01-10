4c01688e43db2befb602a05be53e9e0ea107904d
aviation5770 <aviation5770@gmail.com>
2025-01-10 23:02:19 +0900

    clear page

--- src/pages/clear/Clear.jsx
+++ src/pages/clear/Clear.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 const Clear = () => {
     return(
-        <div className='point-line-mini'>
-            <img src={`/point-line-mini.svg`} alt="Map" />
+        <div>
+            <img src={`/point-line-mini.svg`} alt="Map"  className='point-line-mini'/>
+            <img src={`/imsi-button.svg`} alt='추후 버튼으로 바꿀 예정입니다.' className='clear-2' />
+            <img src={`/clear-1.svg`} alt="대여 창"  className='clear-1'/>
         </div>
     );
 };
