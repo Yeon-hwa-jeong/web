9124fe2e5e65a0cf5031d8c7f8e1c75478fc13a6
aviation5770 <aviation5770@gmail.com>
2025-01-10 20:21:32 +0900

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <Routes>
+        {/* 필요한 라우트를 여기에 추가하세요 */}
+        <Route path="/applicate-list" element={<div>My List Page</div>} />
+        <Route path="/category" element={<div>Category Page</div>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,19 @@
-import { UseNavigate } from 'react-router-dom';
-import './Header.css';
+// Header.tsx
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  const navigate = useNavigate();
-
-  const goToMyList = () =>{
-    navigate('/applicate-list')
-  };
-
   return (
-    <header className = "header">
-      <div className = "header-2">
-        <div className = 'logo'>
-          <a href = "/category" className="header-image left">
-            <img src = {`/hamburger.svg`} alt="Hamburger" /></a>
-            <img src = {`/YeonHwa.svg`} alt="YeonHwaJeong logo" className="header-image center" />
-          <div className="header-image right" onClick={goToMyList}><img src = {`/alram.svg`} alt="alram" className="alram"/></div>
-        </div>
-      </div>
+    <header className="header">
+      <button className="header-button">
+        <img src={`/hamburger.svg`} alt="Left Button" className="left"/>
+      </button>
+      <button className="header-button">
+        <img src={`/YeonHwa.svg`} alt="Center Button" className="center"/>
+      </button>
+      <button className="header-button">
+        <img src={`/alram.svg`} alt="Right Button" className="right"/>
+      </button>
     </header>
   );
 };
