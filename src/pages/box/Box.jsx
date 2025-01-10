65f59d2b89a4fa21430c4c5358858bf8083e9d33
aviation5770 <aviation5770@gmail.com>
2025-01-10 22:45:25 +0900

    save

--- src/pages/box/Box.jsx
+++ src/pages/box/Box.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 const Box = () => {
     return(
-        <div className='point-line'>
-            <img src={`/point-line.svg`} alt="Map" />
+        <div>
+            <img src={`/point-line.svg`} alt="Map"  className='point-line'/>
+            <img src={`/shouthing-list.svg`} alt='신고 및 공공재 충원요청 버튼' className='shouthing-list'/>
+            <img src={`/yeon.svg`} alt='광고표시' className='yeon'/>
         </div>
     );
 };
--- src/pages/door/Door.jsx
+++ src/pages/door/Door.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 
 const Door = () => {
     return (
-        <div className='point-line'>
-            <img src={`/point-line.svg`} alt="Map" />
+        <div>
+            <img src={`/point-line.svg`} alt="line"  className='point-line' />
+            <img src={`/open-the-door.svg`} alt="연암공대 지도를 로드할 예정입니다." className='imsi-map'/>
+            <img src={`/under.svg`} alt="연암공대 부근 사람들이 찾을만한 장소를 로드합니다." className='imsi-map'/>
         </div>
     );
 };
